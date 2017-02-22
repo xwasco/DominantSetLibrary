e498c7e78e86ae39a00a2c7b955d133e728d6e15
Sebastiano Vascon <xwasco@users.noreply.github.com>
2017-02-22 15:09:15 +0100

    Add files via upload

--- setup.m
+++ setup.m
@@ -1,8 +1,6 @@
 addpath(genpath('dynamics'));
 
-if exist('inImDynC','file')~=3
-    fprintf('Compiling Infection-Immunization dynamics...');
-    mex dynamics/inImDynC.cpp -outdir dynamics
-    fprintf('done!\n');
-end
+fprintf('Compiling Infection-Immunization dynamics...\n');
+mex dynamics/inImDynC.cpp -outdir dynamics
+fprintf('done!\n');
     

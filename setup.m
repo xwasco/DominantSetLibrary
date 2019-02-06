11e3a8fd6fea8daee947227b6448be0ff624a470
Sebastiano Vascon <xwasco@users.noreply.github.com>
2019-02-06 18:22:33 +0100

    Update setup.m
    added license

--- setup.m
+++ setup.m
@@ -1,3 +1,8 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Released under: MIT License
+% 2019, Sebastiano Vascon
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 addpath(genpath('dynamics'));
 
 fprintf('Compiling Infection-Immunization dynamics...\n');
